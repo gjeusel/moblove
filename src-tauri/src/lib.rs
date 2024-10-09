35dfaa88a140e1061434cff56c30cf7512bfd097
Jeusel Guillaume <guillaume.jeusel@renewex.co>
2024-10-09 16:12:48 +0200

    add(listing)

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,26 @@
+use tauri_plugin_http::reqwest;
+use serde_json::Value;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[tauri::command]
+async fn fetch_neatpush() -> Result<Value, String> {
+    let url = "https://messy.s3.fr-par.scw.cloud/neatpush.json";
+    let response = reqwest::get(url).await.map_err(|e| e.to_string())?;
+    let json: Value = response.json().await.map_err(|e| e.to_string())?;
+    Ok(json)
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, fetch_neatpush])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,61 @@
 <script lang="ts">
-  import { invoke } from "@tauri-apps/api/core"
+  import { fetch } from "@tauri-apps/plugin-http"
+  // import { invoke } from "@tauri-apps/api/core"
+  import { onMount } from "svelte"
 
-  let name = $state("Boby")
-  let greetMsg = $state("")
-
-  async function greet(event: Event) {
-    event.preventDefault()
-
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    greetMsg = await invoke("greet", { name })
+  interface MangaChapter {
+    url: string
+    num: number
+    timestamp: string
   }
+
+  interface Manga {
+    name: string
+    source: string
+    chapters: MangaChapter[]
+  }
+
+  let mangas = $state<Manga[]>([])
+  let loading = $state<boolean>(true)
+
+  onMount(async () => {
+    try {
+      // mangas = await invoke("fetch_neatpush")
+      const response = await fetch("https://messy.s3.fr-par.scw.cloud/neatpush.json", {
+        mode: "cors",
+      })
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`)
+      }
+      mangas = (await response.json()).reverse()
+    } catch (e) {
+      console.error(e)
+    } finally {
+      loading = false
+    }
+  })
 </script>
 
-<div class="flex flex-col items-center px-4">
+<div class="flex flex-col items-center">
   <img
     src="/icon.png"
-    class="m-4 h-20 w-20 self-center rounded-full p-3 transition-all duration-700 hover:drop-shadow-[0_0_2em_#24c8db] hover:filter"
+    class="mt-2 size-20 self-center rounded-full p-2 transition-all duration-700 hover:drop-shadow-[0_0_2em_#24c8db] hover:filter"
     style="will-change: filter"
     alt="MobLove"
   />
-  <h1 class="text-center text-lg font-normal">Mangas Chapters</h1>
+  <h1 class="my-2 text-center text-lg font-normal">Mangas Chapters</h1>
 
-  <form class="flex items-center space-x-4 text-base font-medium" onsubmit={greet}>
-    <input type="text" class="px-2" placeholder="Enter a name..." bind:value={name} />
-    <button class="btn-cyan py-1 px-2" type="submit">Greet</button>
-  </form>
-  <p>{greetMsg}</p>
+  {#if loading}
+    <div>Loading...</div>
+  {:else}
+    <div class="flex flex-col gap-y-2">
+      {#each mangas as manga}
+        {#each manga.chapters.slice(manga.chapters.length - 2, manga.chapters.length) as chapt}
+          <a href={chapt.url} target="_blank" class="rounded-lg bg-gray-100 dark:bg-gray-700 py-1.5 px-2">
+            {chapt.num} - {manga.name}
+          </a>
+        {/each}
+      {/each}
+    </div>
+  {/if}
 </div>
