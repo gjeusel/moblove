269f1998365cd6ca2a157995e435f11ded67aee4
Jeusel Guillaume <guillaume.jeusel@renewex.co>
2024-10-28 17:41:41 +0100

    wip(layout)

--- src/routes/manga/+layout.svelte
+++ src/routes/manga/+layout.svelte
@@ -1,27 +1,7 @@
 <script lang="ts">
   import { fade } from "svelte/transition"
   import { cubicIn, cubicOut } from "svelte/easing"
-
-  import * as Breadcrumb from "$lib/components/ui/breadcrumb/index.js"
 </script>
-
-<Breadcrumb.Root>
-  <Breadcrumb.List>
-    <Breadcrumb.Item>
-      <Breadcrumb.Link href="/manga">Mangas</Breadcrumb.Link>
-    </Breadcrumb.Item>
-    <!-- {#if focusChapt && focusManga} -->
-    <!--   <Breadcrumb.Separator /> -->
-    <!--   <Breadcrumb.Item> -->
-    <!--     <Breadcrumb.Page> -->
-    <!--       <span class="capitalize">{sanitize.name(focusManga.name)}</span> -->
-    <!--       - -->
-    <!--       <span>{focusChapt.num}</span> -->
-    <!--     </Breadcrumb.Page> -->
-    <!--   </Breadcrumb.Item> -->
-    <!-- {/if} -->
-  </Breadcrumb.List>
-</Breadcrumb.Root>
 
 <div
   in:fade={{ easing: cubicOut, duration: 100, delay: 100 }}
